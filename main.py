77597a1f4d62d7a996f6bcce3c49b011dfad4f43
Kanni <magnificentzeem@gmail.com>
2022-04-26 13:02:16 +0100

    put space between prompt and user input, and print keyword guide for user input

--- main.py
+++ main.py
@@ -49,11 +49,25 @@ elif constant_ei == "no":
     for i in range(number_of_spans):
         beam_spans[i].ei_value = int(input(f"what is the EI value for span {i+1}: "))
 
+# the fixed end moments are calculated based on the loading conditions
+print("Key words for loading condition:"
+      "\nNo loading on span (none)"
+      "\nPoint load at center (P_C)"
+      "\nPoint load at distance 'a' from left end and 'b' from the right end (P_X)"
+      "\nTwo equal point loads, spaced at 1/3 of the total length from each other (P_C_2)"
+      "\nThree equal point loads, spaced at 1/4 of the total length from each other (P_C_3)"
+      "\nUniformly distributed load over the whole length (UDL)"
+      "\nUniformly distributed load over half of the span on the right side (UDL/2_R)"
+      "\nUniformly distributed load over half of the span on the left side (UDL/2_L)"
+      "\nVariably distributed load, with highest point on the right end (VDL_R)"
+      "\nVariably distributed load, with highest point on the left end (VDL_L)"
+      "\nVariably distributed load, with highest point at the center (VDL_C)")
+
 for i in range(number_of_spans):
-    beam_spans[i].span_length = int(input(f"what is the length of span {i + 1}"))
-    beam_spans[i].loading_condition = input(f"what is the loading condition of span {i + 1}")
+    beam_spans[i].span_length = int(input(f"what is the length of span {i + 1}? "))
+    beam_spans[i].loading_condition = input(f"what is the loading condition of span {i + 1}? ")
     if beam_spans[i].loading_condition != "none":
-        beam_spans[i].load = int(input(f"What is the magnitude of the load on span {i + 1}"))
+        beam_spans[i].load = int(input(f"What is the magnitude of the load on span {i + 1}? "))
 
     # The FEM loads(y) and moments(z)
     if beam_spans[i].loading_condition == 'UDL':
@@ -116,7 +130,7 @@ print('''Enter one of the following for support conditions
      "fixed" or "roller" or "pinned" or "joint"''')
 for i in range(number_of_nodes):
     # to get the values of the displacement and forces at each of the nodes (known or unknown)
-    beam_nodes[i].support_condition = input(f"What is the support condition for node {i + 1}")
+    beam_nodes[i].support_condition = input(f"What is the support condition for node {i + 1}? ")
     loaded_node = input(f"Is there any force or moment acting on node {i + 1}? (yes) or (no): ")
     if loaded_node == "yes":
         beam_nodes[i].vertical_loading = int(input(f"Magnitude of vertical force acting on node {i + 1}: "))
